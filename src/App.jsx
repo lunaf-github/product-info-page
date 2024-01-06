23f6a480519327d62b58e8aa26961f0bf1768df9
Fernando <lunaf91@gmail.com>
2024-01-05 18:02:21 -0800

    Initial App layout with semantic html

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const App = () => {
     return (
-        <div>Environment Setup Complete!</div>
+        <div id="app">
+            <nav>Nav</nav>
+            <main>
+                <aside>Side</aside>
+                <article>
+                    <section>Graph</section>
+                    <section>Table</section>
+                </article>
+            </main>
+        </div>
     );
 }
 
